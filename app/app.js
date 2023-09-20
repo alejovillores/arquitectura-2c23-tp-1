0ba95d4d70a77c4305d91006c8d100139218f121
celedituro <cdituro@fi.uba.ar>
2023-09-20 14:58:30 -0300

    feat: add endpoint

--- app/app.js
+++ app/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const axios = require('axios');
+const { XMLParser } = require('fast-xml-parser');
+const { decode } = require('metar-decoder');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+app.get('/metar', async (req, res) => {
+  try {
+    let station = req.query.station;
+    const response = await axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`);
+    
+    const parser = new XMLParser();
+    const parsed = parser.parse(response.data);
+    
+    metar = decode(parsed.response.data.METAR.raw_text);
+    res.status(200).send(metar);
+  } catch (err) {
+    res.status(500).send('Internal Server Error')
+  }
 });
 
 const port = 3000;
