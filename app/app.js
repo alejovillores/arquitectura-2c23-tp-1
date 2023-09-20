2f7c2f4f2d372332252a5165eb16e54eeddcb569
celedituro <cdituro@fi.uba.ar>
2023-09-20 15:16:32 -0300

    feat: handle empty metar

--- app/app.js
+++ app/app.js
@@ -16,10 +16,15 @@ app.get('/metar', async (req, res) => {
     
     const parser = new XMLParser();
     const parsed = parser.parse(response.data);
+    const metar = parsed.response.data.METAR;
+    if (typeof metar === 'undefined') {
+      res.status(400).send('Not METAR found for that station');
+    }
     
-    metar = decode(parsed.response.data.METAR.raw_text);
-    res.status(200).send(metar);
+    const decodedMetar = decode(parsed.response.data.METAR.raw_text);
+    res.status(200).send(decodedMetar);
   } catch (err) {
+    console.error(err)
     res.status(500).send('Internal Server Error')
   }
 });
