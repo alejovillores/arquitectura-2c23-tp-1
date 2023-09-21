b7df55013dcae6694ba5be4f8f7a82442b9c2f2f
alejovillores <avillores@fi.uba.ar>
2023-09-21 18:10:18 -0300

    fix: reverse proxy configuration

--- app/constants.js
+++ app/constants.js
@@ -1,7 +1,9 @@
-const HTTP_200 = 200
-const HTTP_400 = 400
-const HTTP_500 = 500
+const HTTP_200 = 200;
+const HTTP_400 = 400;
+const HTTP_500 = 500;
 
-const SPACEFLIGHT_API_URL = 'https://api.spaceflightnewsapi.net'
+const SPACEFLIGHT_API_URL =
+	'https://api.spaceflightnewsapi.net/v4/articles/?limit=';
+const LIMIT = 5;
 
-module.exports = { HTTP_200, HTTP_400, HTTP_500, SPACEFLIGHT_API_URL }
+module.exports = { HTTP_200, HTTP_400, HTTP_500, SPACEFLIGHT_API_URL, LIMIT };
