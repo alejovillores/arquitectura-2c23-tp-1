9914d615aeafa69bd2f1068e6862e2b20f5df192
celedituro <cdituro@fi.uba.ar>
2023-09-20 22:37:38 -0300

    feat: add endpoint

--- app/app.js
+++ app/app.js
@@ -28,6 +28,23 @@ app.get('/metar', async (req, res) => {
   }
 });
 
+app.get('/spaceflight-news', async (req, res) => {
+  try {
+    let limit = req.query.limit;
+    const response = await axios.get(`https://api.spaceflightnewsapi.net/v4/articles/?limit=${limit}`);
+    
+    news = []
+    response.data.results.forEach((article) => {
+      news.push(article.title);
+    });
+    
+    res.status(HTTP_200).send(news);
+  } catch (err) {
+    console.error(err)
+    res.status(HTTP_500).send('Internal Server Error')
+  }
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
