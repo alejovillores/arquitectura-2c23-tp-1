ec6870eca84dfe941619ce169c263942e60c0fd2
alejovillores <avillores@fi.uba.ar>
2023-09-21 18:15:10 -0300

    feat: add ping endpoint

--- app/app.js
+++ app/app.js
@@ -3,48 +3,61 @@ const axios = require('axios');
 const { XMLParser } = require('fast-xml-parser');
 const { decode } = require('metar-decoder');
 const app = express();
-const { HTTP_200, HTTP_400, HTTP_500, SPACEFLIGHT_API_URL } = require('./constants');
+const {
+	HTTP_200,
+	HTTP_400,
+	HTTP_500,
+	SPACEFLIGHT_API_URL,
+	LIMIT,
+} = require('./constants');
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+/*
+  ping service 
+*/
+app.get('/ping', (_, res) => {
+	res.status(HTTP_200).send("I'm alive!");
 });
 
+/*
+  metar service 
+*/
 app.get('/metar', async (req, res) => {
-  try {
-    let station = req.query.station;
-    const response = await axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`);
-    
-    const parser = new XMLParser();
-    const parsed = parser.parse(response.data);
-    if (!parsed?.response?.data?.METAR?.raw_text) {
-        res.status(HTTP_400).send('Not METAR found for that station');
-    } else {
-      const metar = decode(parsed.response.data.METAR.raw_text);
-      res.status(HTTP_200).send(metar);
-    }
-  } catch (err) {
-    console.error(err)
-    res.status(HTTP_500).send('Internal Server Error')
-  }
+	try {
+		let station = req.query.station;
+		const response = await axios.get(
+			`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`
+		);
+
+		const parser = new XMLParser();
+		const parsed = parser.parse(response.data);
+		if (!parsed?.response?.data?.METAR?.raw_text) {
+			res.status(HTTP_400).send('Not METAR found for that station');
+		} else {
+			const metar = decode(parsed.response.data.METAR.raw_text);
+			res.status(HTTP_200).send(metar);
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(HTTP_500).send('Internal Server Error');
+	}
 });
 
-app.get('/spaceflight_news', async (req, res) => {
-  try {
-    const response = await axios.get(`${SPACEFLIGHT_API_URL}/v4/articles/?limit=5`);
-    
-    news = []
-    response.data.results.forEach((article) => {
-      news.push(article.title);
-    });
+/*
+  spaceflight_news service 
+*/
+app.get('/spaceflight_news', async (_, res) => {
+	try {
+		const response = await axios.get(`${SPACEFLIGHT_API_URL}${LIMIT}`);
 
-    res.status(HTTP_200).send(news);
-  } catch (err) {
-    console.error(err)
-    res.status(HTTP_500).send('Internal Server Error')
-  }
+		news = response.data.results.map((article) => article.title);
+		res.status(HTTP_200).send(news);
+	} catch (err) {
+		console.error(err);
+		res.status(HTTP_500).send('Internal Server Error');
+	}
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+	console.log(`Server listening on port ${port}`);
 });
